aed55b25f53158eff05814697e6c18c2f95191b4
Calah Hansen <calah.hansen@gmail.com>
2019-08-05 21:24:12 -0500

    defined variables and added onclick event listener and score counter output and alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,20 @@
 
-// on-click event that responds to button clicks of the crystal image.
-document.querySelectorAll(".crystal1").forEach(function(node){
-    node.addEventListener("click", function() {
+// define variables
+let scoreCounter = 0;
+const randomNumber = 50;
 
+document.getElementById("random-number").innerHTML = randomNumber;
+
+// on-click event that responds to button clicks of the crystal images associated with class item5.
+document.querySelectorAll(".item5").forEach(function(node){
+    node.addEventListener("click", function() {
+    // Each time the user clicks the crystal the counter goes up by 1 and triggers an alert message.
+        scoreCounter += 10;
       // Clicking the button triggers an alert message.
-      alert("You clicked crystal 1!");
+      alert("You clicked this crystal " + scoreCounter + " times!");
 
     });
   })
-// define variables
+
 
 //
