93bd6809948eb9421a0959170892f6f500c3897f
Calah Hansen <calah.hansen@gmail.com>
2019-08-05 19:58:32 -0500

    game grid layout and css basics

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+// on-click event that responds to button clicks of the crystal image.
+document.querySelectorAll(".crystal1").forEach(function(node){
+    node.addEventListener("click", function() {
+
+      // Clicking the button triggers an alert message.
+      alert("You clicked crystal 1!");
+
+    });
+  })
+// define variables
+
+//
